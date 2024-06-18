379fa7e9daedbac82c91341544348b8f66da220d
mat_mal <matvey_maljanov@rambler.ru>
2024-06-18 12:43:33 +0300

    models register

--- task_tracker/tasks/admin.py
+++ task_tracker/tasks/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import TaskItem, TaskRecord
 
-# Register your models here.
+
+@admin.register(TaskItem)
+class TaskItemAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'priority', 'due_date')
+    search_fields = ('title', 'description')
+    list_filter = ('due_date', 'priority')
+
+
+@admin.register(TaskRecord)
+class TaskRecordAdmin(admin.ModelAdmin):
+    list_display = ('user', 'task', 'date_completed', 'time_spent')
+    search_fields = ('task__title', 'user__username')
+    list_filter = ('date_completed', 'task__priority')
