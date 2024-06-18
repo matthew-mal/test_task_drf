40a4454ca4a8841b9776e73c6dc5f1818cc217a0
mat_mal <matvey_maljanov@rambler.ru>
2024-06-18 12:33:09 +0300

    jwt authenticate

--- task_tracker/task_tracker/urls.py
+++ task_tracker/task_tracker/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from tasks.views import SummaryViewSet, TaskItemViewSet, TaskRecordViewSet
-
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 router = DefaultRouter()
 router.register(r'task', TaskItemViewSet)
@@ -12,4 +12,6 @@ router.register(r'summary', SummaryViewSet, basename='summary')
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
